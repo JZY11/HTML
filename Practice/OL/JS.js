4896fe90e85b22e5deecfb17cb2754052634c1fe
Tony.Jaa <1291813139@qq.com>
2017-04-14 16:03:30 +0800

    aaa

---
+++ Practice/OL/JS.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Tony.Jaa on 2017/4/14.
+ */
+var box = document.g
---
+++ Practice/OL/WebSite.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Tony.Jaa on 2017/4/14.
+ */
+document.write('<h1> 你好.JS</h1>');
+alert('你好');
+var button = document.getElementsByTagName('button');
+button.style.color = "red";
+button.style.background ="blue";
+var h1 = document.getElementById('h1');
+h1.style.color = "green";
+
